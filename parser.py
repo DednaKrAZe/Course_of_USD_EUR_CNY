9ff87d18b2dac9d131bfc88fb42ba5c77255f189
JoeBiden1942 <ilyakorgin@gmail.com>
2024-08-22 00:18:42 +0300

    final version

--- parser.py
+++ parser.py
@@ -1,12 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-def tofloat(crs):
-    crs=crs[-1].text
-    crs=crs.replace(',','.')
-    crs=float(crs)
-    return crs
-
 headers={
     "Accept":"*/*",
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
